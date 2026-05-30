8d37ac31719ed82bab79f7e32f6f7c8761a3559f
GO <kiisky05@gmail.com>
2026-05-31 01:52:10 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 "https://fbetpczzpwbutmgxmqqy.supabase.co";
 
 const SUPABASE_KEY =
-"sb_secret_QZ8VpfkbgzXuoDIPXw1GvQ_wvJLt5sr";
+"-----";
 
 const supabaseClient =
 supabase.createClient(
