5aa6bade2888a9803590243207730a4badf61d11
GO <kiisky05@gmail.com>
2026-05-31 01:53:34 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 "https://fbetpczzpwbutmgxmqqy.supabase.co";
 
 const SUPABASE_KEY =
-"-----";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZiZXRwY3p6cHdidXRtZ3htcXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjc4OTgsImV4cCI6MjA5NTcwMzg5OH0.4X5va5WwRTPN-HkWD4uHWBzR680NrTTNKCy4PwjNKak";
 
 const supabaseClient =
 supabase.createClient(
